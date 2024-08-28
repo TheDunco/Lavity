9eec833f56af9e27b2a611dabe23226223d3ce63
TheDunco <duncvan@gmail.com>
2024-08-28 16:52:22 -0400

    updated the tutorial level a bit

--- Lavity/scenes/Entities/Enemies/Lanternfly/Lanternfly.gd
+++ Lavity/scenes/Entities/Enemies/Lanternfly/Lanternfly.gd
@@ -8,8 +8,7 @@ func _process(delta):
 	var areas = $DamageArea.get_overlapping_areas()
 	if vampiricBoost > 0.1:
 		vampiricBoost -= 0.08 * delta
-	var lightScale = 1/log(vampiricBoost/2)/2
-	$Lanternlight.scale = Vector2(lightScale, lightScale)
+	$Lanternlight.scale = Vector2(vampiricBoost, vampiricBoost)
 		
 	for area in areas:
 		if area.name == "DamageableArea":
--- Lavity/scenes/Entities/Passive/Firefly.gd
+++ Lavity/scenes/Entities/Passive/Firefly.gd
@@ -11,21 +11,18 @@ class_name Firefly
 @export var maxRandLightSeconds := 4.0
 
 var lookThreshold := 40.0
-var lavityLightLight: PointLight2D = null
+@onready var lavityLightLight: PointLight2D = lavityLight.find_child("LavityLightLight", false)
 
 func applyRandomImpulse() -> void:
 	velocity += Vector2(randf_range(-impulse, impulse), randf_range(-impulse, impulse))
 	look_at(velocity.normalized())
 	
-func _ready() -> void:
-	lavityLightLight = lavityLight.find_child("LavityLightLight", false)
-
 func _physics_process(_delta):
 	velocity = $VelocityComponent.handleExistingVelocity(self.velocity)
 	move_and_slide()
 	
 func _process(_delta):
-	$FlippingSprite.speed_scale = $VelocityComponent.getAnimationSpeed(self.velocity)
+	#$FlippingSprite.speed_scale = $VelocityComponent.getAnimationSpeed(self.velocity)
 
 	# Look towards the direction of travel
 	if abs(velocity.x) > lookThreshold or abs(velocity.y) > lookThreshold:
