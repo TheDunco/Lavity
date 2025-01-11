4a2968e10e3dd569a4407888ed6f329e9082fa67
TheDunco <duncvan@gmail.com>
2025-01-11 15:54:42 -0500

    trimming and cleanup

--- Lavity/scenes/Objects/LavityLight.gd
+++ Lavity/scenes/Objects/LavityLight.gd
@@ -3,7 +3,6 @@ class_name LavityLight
 
 @export var energy := 1.0
 @export var MaxRgb := COLOR_UTILS.MAX_RAND_RGB
-@export var canChangeColor := false
 @export var randomizeColorOnReady := false
 @export_color_no_alpha var initColor: Color
 
@@ -18,7 +17,3 @@ func _ready():
  
 func set_energy(val: float):
 	light.energy = val
-
-func _unhandled_input(event):
-	if event.is_action_pressed("change_color") and canChangeColor:
-		light.color = COLOR_UTILS.randColor(MaxRgb)
