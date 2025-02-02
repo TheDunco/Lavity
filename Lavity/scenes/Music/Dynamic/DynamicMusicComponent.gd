7d3712d1dd03037a2d0e4972745f2c07cdb95e38
TheDunco <duncvan@gmail.com>
2025-02-02 09:42:36 -0500

    Dynamic music v2, more ambient/ethereal overall. Hints to Light

--- Lavity/scenes/Music/Dynamic/DynamicMusicComponent.gd
+++ Lavity/scenes/Music/Dynamic/DynamicMusicComponent.gd
@@ -1,16 +1,18 @@
 extends AudioStreamPlayer
 class_name DynamicMusicComponent
 
+const MIN_VOLUME = -80
+
 @export var ColorTracks: Array[AudioStreamPlayer] = []
+@onready var swell: AudioStreamPlayer = $Swell
 
 func loop(song: AudioStreamPlayer):
 	song.stop()
 	song.play()
 	
 func _ready():
-	# Autoplay everything but the death track
 	for i in range(ColorTracks.size()):
-		ColorTracks[i].volume_db = -80
+		ColorTracks[i].volume_db = MIN_VOLUME
 		ColorTracks[i].connect("finished", func(): loop(ColorTracks[i]))
 
 func enable():
@@ -22,9 +24,10 @@ func disable():
 		ColorTracks[i].stop()
 
 func scoreToVolume(score: float, trackIndex: int):
-	ColorTracks[trackIndex].volume_db = remap(score, 0.0, 1.0, -80, 0)
+	ColorTracks[trackIndex].volume_db = remap(score, 0.0, 2.0, MIN_VOLUME , 0)
 
 # Synisthesia
 func setDynamicTrackVolume(color: Color):
 	for i in range(ColorTracks.size()):
-		scoreToVolume(ColorUtils.scoreColorLikeness(color, ColorUtils.colorsArray[i]), i)
+		var whiteScore = ColorUtils.scoreColorLikeness(color, Color.WHITE)
+		scoreToVolume(whiteScore + ColorUtils.scoreColorLikeness(color, ColorUtils.colorsArray[i]), i)	
