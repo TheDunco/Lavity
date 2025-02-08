bd3b46f19e701a204d965f1da4bf3c250a359f36
TheDunco <duncvan@gmail.com>
2025-02-07 22:40:00 -0500

    Version bump and slight text changes

--- Lavity/scenes/Worlds/Intro/IntroCalibrationState.gd
+++ Lavity/scenes/Worlds/Intro/IntroCalibrationState.gd
@@ -8,9 +8,9 @@ const moteScene = preload("res://scenes/Objects/Mote.tscn")
 var currentHeroTextIndex := -1
 const heroText: Array[String] = [
 	"\n\n[center]Welcome, [pulse]Cosmo[/pulse], to the world of [wave]Lavity[/wave][/center]",
-	"\n\n[center]We'll need to adjust your eyes and ears to the environment[/center]",
-	"\n\n[center]Use [code]Right Arrow[/code] to skip or [code]Left Arrow[/code] to go back through this text[/center]",
-	"\n\n[center]Open the menu with [code]Escape[/code] and navigate to Options with [code]O[/code]",
+	"\n\n[center]We'll need to adjust your eyes and ears to the environment...[/center]",
+	"\n[center]Use [code]Right Arrow[/code] to skip or [code]Left Arrow[/code] to go back through this text[/center]",
+	"\n[center]Open the menu with [code]Escape[/code] and navigate to Options with [code]O[/code]",
 	"\n[center]Adjust the Brightness slider until the brightest inner ring of your light is just gone",
 	"\n[center]Adjust the Darkness slider until the background color matches the reference[/center]",
 	"[center]The glow settings are preference, but don't go overboard for the best experience[/center]",
@@ -52,4 +52,3 @@ func _unhandled_key_input(event: InputEvent) -> void:
 	if event.is_action_released("skip_back"):
 		currentHeroTextIndex -= 2
 		nextHeroText()
-	
