3fea27d64f94b66e9f923e0b75a55f19d3f4075d
TheDunco <duncvan@gmail.com>
2025-02-08 12:03:29 -0500

    Split out volume sliders for finer control

--- Lavity/scenes/Ui/Menu.gd
+++ Lavity/scenes/Ui/Menu.gd
@@ -8,8 +8,8 @@ class_name Menu
 @export var showMainMenuButton := true
 @export var visibleByDefault := false
 
-@export var buttonTweenIntensity := 1.1
-@export var buttonTweenDuration := 0.1
+@export var buttonTweenIntensity := 1.05
+@export var buttonTweenDuration := 0.08
 
 @export_category("Effect Implementaitons")
 @export var _directionalDarkness: DirectonalDarkness
@@ -24,10 +24,12 @@ class_name Menu
 @onready var glowStrengthSlider: HSlider = find_child("GlowStrengthSlider")
 @onready var bloomSlider: HSlider = find_child("BloomSlider")
 @onready var masterVolumeSlider: HSlider = find_child("MasterVolumeSlider")
+@onready var musicVolumeSlider: HSlider = find_child("MusicVolumeSlider")
+@onready var sfxVolumeSlider: HSlider = find_child("SfxVolumeSlider")
 
-@onready var masterBusIndex := AudioServer.get_bus_index("Master")
-@onready var sfxBusIndex := AudioServer.get_bus_index("SFX")
-@onready var musicBusIndex := AudioServer.get_bus_index("Music")
+@onready var masterBusIndex: int = AudioServer.get_bus_index("Master")
+@onready var musicBusIndex: int = AudioServer.get_bus_index("Music")
+@onready var sfxBusIndex: int = AudioServer.get_bus_index("SFX")
 
 @onready var Buttons = find_children("", "Button", true)
 @onready var Sliders = find_children("", "Slider", true)
@@ -39,8 +41,8 @@ class_name Menu
 @onready var options = $Options
 @onready var controls = $Controls
 @onready var play = $Play
-@onready var calibrationReference: TextureButton = $Options/AspectRatioContainer/MarginContainer/VBoxContainer/Darkness/CalibrationReference
 
+@onready var calibrationReference: TextureButton = find_child("CalibrationReference")
 func setShowCalibrationReference(shown: bool):
 	calibrationReference.visible = shown
 
@@ -77,6 +79,16 @@ func loadOptions():
 	if masterVolume and masterVolume > masterVolumeSlider.min_value:
 		masterVolumeSlider.value = masterVolume
 		_on_master_volume_slider_value_changed(masterVolume)
+
+	var musicVolume = Settings.getSetting("MUSIC_VOLUME")
+	if musicVolume and musicVolume > musicVolumeSlider.min_value:
+		musicVolumeSlider.value = musicVolume
+		_on_music_volume_slider_value_changed(musicVolume)
+
+	var sfxVolume = Settings.getSetting("SFX_VOLUME")
+	if sfxVolume and sfxVolume > sfxVolumeSlider.min_value:
+		sfxVolumeSlider.value = sfxVolume
+		_on_sfx_volume_slider_value_changed(sfxVolume)
 
 func _ready():
 	loadOptions()
@@ -153,14 +165,23 @@ func _on_bloom_slider_value_changed(value: float) -> void:
 	if _worldEnvironment:
 		_worldEnvironment.environment.glow_bloom = value
 
-func _on_master_volume_slider_value_changed(value: float):
-	Settings.setSetting("MASTER_VOLUME", value)
+func setVolume(value: float, slider: HSlider, busIndex: int, settingName: String):
+	Settings.setSetting(settingName, value)
 	GlobalSfx.playButtonHover()
-	if value == masterVolumeSlider.min_value:
-		AudioServer.set_bus_mute(masterBusIndex, true)
+	if value == slider.min_value:
+		AudioServer.set_bus_mute(busIndex, true)
 	else:
-		AudioServer.set_bus_mute(masterBusIndex, false)
-		AudioServer.set_bus_volume_db(masterBusIndex, value)
+		AudioServer.set_bus_mute(busIndex, false)
+		AudioServer.set_bus_volume_db(busIndex, value)
+
+func _on_music_volume_slider_value_changed(value: float) -> void:
+	setVolume(value, musicVolumeSlider, musicBusIndex, "MUSIC_VOLUME")
+
+func _on_sfx_volume_slider_value_changed(value: float) -> void:
+	setVolume(value, sfxVolumeSlider, sfxBusIndex, "SFX_VOLUME")
+
+func _on_master_volume_slider_value_changed(value: float):
+	setVolume(value, masterVolumeSlider, masterBusIndex, "MASTER_VOLUME")
 
 func _on_fullscreen_pressed() -> void:
 	if DisplayServer.window_get_mode() != DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN:
@@ -196,6 +217,13 @@ func _on_reset_to_default_pressed() -> void:
 	_on_bloom_slider_value_changed(DEFAULTS.bloom)
 	bloomSlider.value = DEFAULTS.bloom
 
+	_on_master_volume_slider_value_changed(0)
+	masterVolumeSlider.value = 0
+	_on_sfx_volume_slider_value_changed(sfxVolumeSlider.max_value)
+	sfxVolumeSlider.value = sfxVolumeSlider.max_value
+	_on_music_volume_slider_value_changed(musicVolumeSlider.max_value)
+	musicVolumeSlider.value = musicVolumeSlider.max_value
+
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_esc"):
 		GlobalSfx.playTonalClick(0.5)
@@ -211,3 +239,4 @@ func _on_controls_pressed() -> void:
 
 func _on_calibration_pressed() -> void:
 	GameFlow.switchScene("res://scenes/Worlds/Intro/Calibration.tscn")
+	GameFlow.switchToMenuMusic()
